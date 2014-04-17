275b4c805c4e3a4e4da2399c8d19a078103b668b
Egor Hamaliy <egor.hamaliy@gmail.com>
2014-04-17 23:20:17 +0300

    Errors handling

--- app/datatables/roles_datatable.rb
+++ app/datatables/roles_datatable.rb
@@ -8,7 +8,6 @@ class RolesDatatable
   def as_json(options = {})
     {
       aaData: data
-      # aaData: [['base1', 'base2'], ['base3', 'base4']]
     }
   end
 
@@ -33,26 +32,12 @@ private
   def fetch_roles
     topology_list = ChefHelper.fetch_topology
 
-    # @roles_list = {}
+    roles_list = Hash.new
 
-    # if topology_list.has_key?(:error)
-    #   flash[:error] = topology_list[:error]
-    # else
-    #   @roles_list = topology_list[:roles_list]
-    # end
+    unless topology_list.has_key?(:error)
+      roles_list = topology_list[:roles_list]
+    end
 
-  #   topology_list = {:roles_list=>
-  # {"apache"=>["test1.test.com"],
-  #  "base"=>
-  #   ["prod1.test.com", "stg2.test.com", "test1.test.com", "node2.test.com"],
-  #  "database"=>["stg2.test.com", "test1.test.com"],
-  #  "demo"=>["stg2.test.com", "test2.test.com"],
-  #  "load_balancer"=>["test1.test.com", "test2.test.com", "stg1.test.com"],
-  #  "railsapp"=>["stg1.test.com"],
-  #  "test"=>["prod1.test.com", "stg2.test.com"],
-  #  "web_server"=>["test2.test.com"],
-  #  "webapp"=>["prod1.test.com", "node2.test.com", "stg1.test.com"]}}
-
-    topology_list[:roles_list]
+    roles_list
   end
 end
