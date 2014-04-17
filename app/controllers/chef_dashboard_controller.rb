5b976fb35a47a2f056b897eaad15275c68db20c0
Egor Hamaliy <egor.hamaliy@gmail.com>
2014-04-17 17:30:01 +0300

    Exception handling and errors flashing added

--- app/controllers/chef_dashboard_controller.rb
+++ app/controllers/chef_dashboard_controller.rb
@@ -1,7 +1,13 @@
-require 'chef_helper'
-
 class ChefDashboardController < ApplicationController
   def index
-    @roles_list = ChefHelper.fetch_topology
+    topology_list = ChefHelper.fetch_topology
+    @roles_list = {}
+
+    binding.pry
+    if topology_list.has_key?(:error)
+      flash[:error] = topology_list[:error]
+    else
+      @roles_list = topology_list[:roles_list]
+    end
   end
 end
--- lib/chef_helper.rb
+++ lib/chef_helper.rb
@@ -3,28 +3,32 @@ require 'chef/rest'
 module ChefHelper
   class << self
     def fetch_topology
-      chef_server_url = CHEF_CONFIG['server_url']
-      client_name = CHEF_CONFIG['client_name']
-      file_name = File.join(Rails.root, 'chef_config', CHEF_CONFIG['client_key_file_name'])
+      topology_list = {}
+      begin
+        chef_server_url = CHEF_CONFIG['server_url']
+        client_name = CHEF_CONFIG['client_name']
+        file_name = File.join(Rails.root, 'chef_config', CHEF_CONFIG['client_key_file_name'])
 
+        rest = Chef::REST.new(chef_server_url)#, client_name, file_name)
+        roles = rest.get_rest("/roles/")
 
-      rest = Chef::REST.new(chef_server_url, client_name, file_name)
-      roles = rest.get_rest("/roles/")
+        roles.keys.each do |role|
+          search_result = rest.get_rest("/search/node?q=role:#{role}")
+          nodes_list = search_result["rows"]
 
-      roles_list = {}
-      roles.keys.each do |role|
-        search_result = rest.get_rest("/search/node?q=role:#{role}")
-        nodes_list = search_result["rows"]
+          nodes_arr = []
+          nodes_list.each do |node|
+            nodes_arr << node.name
+          end
 
-        nodes_arr = []
-        nodes_list.each do |node|
-          nodes_arr << node.name
+          roles_list[role] = nodes_arr
+          topology_list[:roles_list] = roles_list
         end
-
-        roles_list[role] = nodes_arr
+      rescue Exception => ex
+        topology_list[:error] = "Processing failed. Error: #{ex.message}"
       end
 
-      roles_list
+      topology_list
     end
   end
 end
